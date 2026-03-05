5e78325b1cd33c95a1b1a7c793a815610415f9e9
lorypelli <lorypelli@proton.me>
2026-03-05 21:10:43 +0100

    api key not valid if email or api key missing

--- src/utils/is_api_key_valid.php
+++ src/utils/is_api_key_valid.php
@@ -2,6 +2,11 @@
 function is_api_key_valid(): bool
 {
     global $resend;
+    $email = $_ENV['EMAIL'] ?? '';
+    $api_key = $_ENV['APIKEY'] ?? '';
+    if (!$email || !$api_key) {
+        return false;
+    }
     try {
         $resend->emails->send([
             'from' => $_ENV['EMAIL'],
