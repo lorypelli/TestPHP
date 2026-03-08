d171cff695b1527500e2e78c8198086809ac7616
lorypelli <lorypelli@proton.me>
2026-03-08 22:02:28 +0100

    some fixes

--- golang/src/main.go
+++ golang/src/main.go
@@ -17,31 +17,41 @@ type Response struct {
 	Code   string `json:"code"`
 }
 
+var (
+	UUID_REGEX  = regexp.MustCompile(`^[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[1-8][0-9A-Fa-f]{3}-[ABab89][0-9A-Fa-f]{3}-[0-9A-Fa-f]{12}$`)
+	EMAIL_REGEX = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	CODE_REGEX  = regexp.MustCompile(`^\d{6}$`)
+)
+
 func main() {
-	server := fasthttp.Server{}
-	response := Response{}
-	client := fasthttp.Client{}
-	req := fasthttp.AcquireRequest()
-	res := fasthttp.AcquireResponse()
-	defer fasthttp.ReleaseRequest(req)
-	defer fasthttp.ReleaseResponse(res)
+	server := &fasthttp.Server{}
+	client := &fasthttp.Client{}
 	server.Handler = func(ctx *fasthttp.RequestCtx) {
 		path := string(ctx.Path())
 		bytes, err := base64.StdEncoding.DecodeString(path[1:])
-		fmt.Println(path[1:], string(bytes))
 		if err == nil {
-			if err := json.Unmarshal(bytes, &response); err == nil {
+			response := &Response{}
+			if err := json.Unmarshal(bytes, response); err == nil {
 				userID := response.UserID
 				email := response.Email
 				code := response.Code
-				isValidUUID := regexp.MustCompile(`^[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[1-8][0-9A-Fa-f]{3}-[ABab89][0-9A-Fa-f]{3}-[0-9A-Fa-f]{12}$`).MatchString(userID)
-				isValidEmail := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`).MatchString(email)
-				isValidCode := regexp.MustCompile(`^\d{6}$`).MatchString(code)
+				isValidUUID := UUID_REGEX.MatchString(userID)
+				isValidEmail := EMAIL_REGEX.MatchString(email)
+				isValidCode := CODE_REGEX.MatchString(code)
 				if isValidUUID && isValidEmail && isValidCode {
+					req := fasthttp.AcquireRequest()
+					res := fasthttp.AcquireResponse()
+					args := fasthttp.AcquireArgs()
+					defer fasthttp.ReleaseRequest(req)
+					defer fasthttp.ReleaseResponse(res)
+					defer fasthttp.ReleaseArgs(args)
 					ctx.Request.CopyTo(req)
 					req.Header.SetMethod(fasthttp.MethodPost)
 					req.SetRequestURI("http://php/verify")
-					req.SetBodyString(fmt.Sprintf("id=%s&email=%s&code=%s", userID, email, code))
+					args.Set("id", userID)
+					args.Set("email", email)
+					args.Set("code", code)
+					req.SetBodyString(args.String())
 					req.Header.SetContentType("application/x-www-form-urlencoded")
 					if err := client.Do(req, res); err == nil {
 						res.CopyTo(&ctx.Response)
@@ -58,7 +68,7 @@ func main() {
 		}
 		ctx.Redirect("/login", fasthttp.StatusFound)
 	}
-	preforkServer := prefork.New(&server)
+	preforkServer := prefork.New(server)
 	if err := preforkServer.ListenAndServe(":80"); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
