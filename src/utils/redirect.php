89f61024f0ed5b715354af3556a9cd2363fa0a91
lorypelli <lorypelli@proton.me>
2026-05-10 14:38:10 +0200

    added optional `replace` param

--- src/utils/redirect.php
+++ src/utils/redirect.php
@@ -1,5 +1,5 @@
 <?php
 function redirect(string $url, int $code = 0): void
 {
-    header(sprintf('Location: %s', $url), response_code: $code);
+    header(sprintf('Location: %s', $url), true, $code);
 }
