9db13293fc9d12249d66464ed81e748f0a498f81
lorypelli <lorypelli@proton.me>
2026-06-27 19:41:49 +0200

    fix avatar

--- src/avatar/index.php
+++ src/avatar/index.php
@@ -3,6 +3,7 @@ $root = $_SERVER['DOCUMENT_ROOT'];
 require_once sprintf('%s/vendor/autoload.php', $root);
 require_once sprintf('%s/src/utils/show_fallback_avatar.php', $root);
 require_once sprintf('%s/src/classes/UserTable.php', $root);
+$cookies = require_once sprintf('%s/src/cookies/index.php', $root);
 Dotenv\Dotenv::createImmutable($root)->load();
 header('Content-Type: image/gif');
 $users = new UserTable();
